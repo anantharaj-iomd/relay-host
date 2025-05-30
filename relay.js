60ad3cd994234a11d3a2c083342624cd868f7357
anantharaj-iomd <anantharaj_ext@iomd.info>
2025-05-30 17:46:40 +0530

    Create relay.js

---
+++ relay.js
@@ -0,0 +1,16 @@
+(() => {
+  const CHANNEL_NAME = "nodeAI-CheckoutUI";
+
+  try {
+    const bc = new BroadcastChannel(CHANNEL_NAME);
+
+    bc.onmessage = (event) => {
+      console.log("[nodeAI Relay] Forwarding message from BroadcastChannel to window:", event.data);
+      window.postMessage(event, "*");
+    };
+
+    console.log(`[nodeAI Relay] Listening on BroadcastChannel: ${CHANNEL_NAME}`);
+  } catch (e) {
+    console.error("[nodeAI Relay] Error setting up BroadcastChannel:", e);
+  }
+})();
